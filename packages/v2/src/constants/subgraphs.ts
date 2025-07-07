b08842d0cadd88f988a9eae8a9e98b4ce334518e
jong-un <36259838+zwong91@users.noreply.github.com>
2025-07-07 14:44:48 +0800

    v2 addr subgraph

--- packages/v2/src/constants/subgraphs.ts
+++ packages/v2/src/constants/subgraphs.ts
@@ -10,9 +10,9 @@ export const DEX_V2_SUBGRAPH: { [chainId in ChainId]: string } = {
   [ChainId.ARB_GOERLI]:
     'https://api.thegraph.com/subgraphs/name/traderjoe-xyz/joe-v2-arb-goerli',
   [ChainId.BNB_CHAIN]:
-    'https://api.thegraph.com/subgraphs/name/traderjoe-xyz/joe-v2-bnb',
+    'https://api.studio.thegraph.com/query/114739/entysquare-dex-bsc/version/latest',
   [ChainId.BNB_TESTNET]:
-    'https://api.thegraph.com/subgraphs/name/traderjoe-xyz/joe-v2-bnbtest',
+    'https://api.studio.thegraph.com/query/114739/entysquare-dex-bsc-testnet/version/latest',
   [ChainId.ETHEREUM]: '',
   [ChainId.MANTLE]: '',
   [ChainId.BERACHAIN]: '',
